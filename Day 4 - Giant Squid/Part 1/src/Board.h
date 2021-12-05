82d82fa166b82b101c020671fcb7a1de3f2248c9
diff-arch <10381015+diff-arch@users.noreply.github.com>
2021-12-05 12:51:52 +0100

    Add files via upload

---
+++ Day 4 - Giant Squid/Part 1/src/Board.h
@@ -0,0 +1,54 @@
+//
+//  Board.h
+//  ADVENT OF CODE 2021: Day 4 - Giant Squid (Part 1)
+//  
+//  Created by diff-arch on 04/12/2021.
+//
+
+#ifndef Board_h
+#define Board_h
+
+#include <iostream>
+#include <vector>
+
+#include "Utils.h"
+
+struct Cell {
+	
+  Cell();
+	Cell(int _value);
+	~Cell();
+	
+	friend std::ostream& operator <<(std::ostream& os, const Cell& c);
+	
+	int value;
+	bool marked;
+
+};
+
+
+struct Board {
+	
+	Board(int _width, int _height);
+	~Board();
+	
+	friend std::ostream& operator <<(std::ostream& os, const Board& b);
+	
+	void addRow(const std::vector<int> values);
+	std::vector<int> getCellValues() const;
+	int getIndex(int value) const;
+	bool markCell(int value);
+	std::vector<Cell> getUnmarked() const;
+	int sumUnmarked() const;
+	int getSize() const;
+	int checkRows() const;
+	int checkColumns() const;
+	bool bingo() const;
+	
+	int width, height;
+	std::vector<Cell> cells;
+
+};
+
+
+#endif
