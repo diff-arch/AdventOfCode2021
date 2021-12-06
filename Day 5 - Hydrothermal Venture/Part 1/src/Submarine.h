8ce3f6165984564e904c2ee6cb107b3d63acb638
diff-arch <10381015+diff-arch@users.noreply.github.com>
2021-12-06 14:23:14 +0100

    Add files via upload

---
+++ Day 5 - Hydrothermal Venture/Part 1/src/Submarine.h
@@ -0,0 +1,40 @@
+//
+//  Submarine.h
+//  ADVENT OF CODE 2021: Day 5 - Hydrothermal Venture (Part 1)
+//  
+//  Created by diff-arch on 05/12/2021.
+//
+
+#ifndef Submarine_h
+#define Submarine_h
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+#include <algorithm>
+
+#include "Grid.h"
+#include "Utils.h"
+
+
+struct Submarine {
+	
+	Submarine();
+	~Submarine();
+	
+	friend std::ostream& operator <<(std::ostream& os, const Submarine& sub);
+	
+	std::vector< std::pair<Node, Node> > parseVentData(const char* filepath);
+	void evaluateVents(const char* filepath);
+	int countVentOverlaps(int min=2);
+	
+	int max_vent_x = 0;
+	int max_vent_y = 0;
+	
+	Grid grid;
+	
+};
+
+
+#endif
---
+++ Day 5 - Hydrothermal Venture/Part 1/src/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  ADVENT OF CODE 2021: Day 5 - Hydrothermal Venture (Part 1)
+//  
+//  Created by diff-arch on 05/12/2021.
+//
+//  Goal:    Consider only horizontal and vertical lines. 
+//				 	 At how many points do at least two lines overlap?
+//
+//  Compile: g++ -std=c++11 -o bin/a.out src/*.cpp
+//  Run:     ./bin/a.out
+//
+
+#include <iostream>
+
+#include "Submarine.h"
+
+
+int main() {
+	
+	const char* fpath = "../bin/data/example-data.txt";  // insert your path
+
+	Submarine sub = Submarine();
+	sub.evaluateVents(fpath);
+	
+	std::cout << sub.grid << "\n\n";
+	std::cout << "Number of Two or More Overlaps: " << sub.countVentOverlaps() << "\n";
+	
+}
